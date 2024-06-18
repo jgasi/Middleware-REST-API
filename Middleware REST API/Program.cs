cba5f6464b1f988d9fe75c5a3eda388cdc7b1d0d
juraj <juraj@DESKTOP-QAJPGCA>
2024-06-18 13:38:28 +0200

    Project structure has been set. Required NuGet Packages have been installed. Created a Product class in the Model folder aswell as the database context class. Implemented the IProductRepository interface.

--- Middleware REST API/Program.cs
+++ Middleware REST API/Program.cs
@@ -1,8 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using Middleware_REST_API.Model;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Add the database context
+builder.Services.AddDbContext<ContextDb>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
---
+++ Middleware REST API/Repositories/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using Middleware_REST_API.Model;
+
+namespace Middleware_REST_API.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAllProducts();
+        Task<Product> GetProductById(int id);
+        Task<IEnumerable<Product>> GetProductsByCategory(string category);
+        Task<IEnumerable<Product>> GetProductsByPriceRange(decimal minPrice, decimal maxPrice);
+        Task<IEnumerable<Product>> SearchProductsByName(string name);
+    }
+}
